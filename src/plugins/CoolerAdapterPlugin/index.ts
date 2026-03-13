3045e60ab1106943013bea393601f91b6f3490f2
Lars Grønvold <lars.gronvold@gmail.com>
2026-03-13 16:57:47 +0100

    CoolerAdapter file type guessing and track type support

--- src/plugins/CoolerAdapterPlugin/index.ts
+++ src/plugins/CoolerAdapterPlugin/index.ts
@@ -1,6 +1,7 @@
 import Plugin from '@jbrowse/core/Plugin'
 import type PluginManager from '@jbrowse/core/PluginManager'
 import { AdapterType } from '@jbrowse/core/pluggableElementTypes'
+import { getFileName } from '@jbrowse/core/util/tracks'
 import CoolerAdapterConfigSchema from './configSchema'
 
 export default class CoolerAdapterPlugin extends Plugin {
@@ -14,7 +15,45 @@ export default class CoolerAdapterPlugin extends Plugin {
           displayName: 'Cooler adapter',
           configSchema: CoolerAdapterConfigSchema,
           getAdapterClass: () => import('./CoolerAdapter').then(r => r.default),
+          adapterMetadata: {
+            supportedFileExtensions: ['mcool', 'cool'],
+            category: 'Hi-C',
+            description: 'Cooler/mcool Hi-C contact matrix adapter',
+          },
         }),
+    )
+
+    pluginManager.addToExtensionPoint(
+      'Core-guessAdapterForLocation',
+      (adapterGuesser: Function) => {
+        return (
+          file: { uri: string; locationType: string },
+          index: unknown,
+          adapterHint: string,
+        ) => {
+          const fileName = getFileName(file)
+          if (
+            (/\.m?cool$/i.test(fileName) && !adapterHint) ||
+            adapterHint === 'CoolerAdapter'
+          ) {
+            return {
+              type: 'CoolerAdapter',
+              coolerLocation: file,
+            }
+          }
+          return adapterGuesser(file, index, adapterHint)
+        }
+      },
+    )
+
+    pluginManager.addToExtensionPoint(
+      'Core-guessTrackTypeForLocation',
+      (trackTypeGuesser: Function) => {
+        return (adapterName: string) =>
+          adapterName === 'CoolerAdapter'
+            ? 'HicTrack'
+            : trackTypeGuesser(adapterName)
+      },
     )
   }
 }
